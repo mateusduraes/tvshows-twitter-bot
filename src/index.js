242e5a2759cb9b20ceaf611fd69c1bbdc03fdfe4
mduraes1994 <mduraes1994@gmail.com>
2018-05-30 23:33:46 -0300

    Separating key to another file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { users } from './users';
 import Twit from 'twit';
+import { apiKey } from './key';
 
-const T = new Twit({
-  consumer_key: '...',
-  consumer_secret: '...',
-  access_token: '...',
-  access_token_secret: '...',
-});
-
+const T = new Twit(apiKey);
 const status = 'Consuming again with Twitter API... just a test';
+
 T.post('statuses/update', { status }, (err, data, response) => {
   console.log('data', data);
 });
---
+++ src/key.js
@@ -0,0 +1,6 @@
+export const apiKey = {
+  consumer_key: '...',
+  consumer_secret: '...',
+  access_token: '...',
+  access_token_secret: '...',
+};
