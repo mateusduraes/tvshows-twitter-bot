ecbca9bfac1cb5cd8fdc7f409bed0a958a81a2f5
mduraes1994 <mduraes1994@gmail.com>
2018-05-30 23:40:04 -0300

    implemeting the stream to listen new tweet and retweet it

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import Twit from 'twit';
 import { apiKey } from './key';
 
 const T = new Twit(apiKey);
-const status = 'Consuming again with Twitter API... just a test';
 
-T.post('statuses/update', { status }, (err, data, response) => {
-  console.log('data', data);
+const stream = T.stream('status/filter', { follow: users } );
+stream.on('tweet', (tweet) => {
+  console.log('tweet', tweet);
+  if (!users.includes(tweet.user.id_str)) return;
+  T.post('statuses/retweet/:id', { id: tweet.id_str }, (err, data, response) => {
+    console.log('data', data);
+  });
 });
